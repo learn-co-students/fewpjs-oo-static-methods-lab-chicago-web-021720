eadc5b09c26fec1d09f71541ea270c976e34b03d
GreatDeveloper66 <ashaffe@gmail.com>
2020-04-14 18:20:21 -0500

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,25 @@
 class Formatter {
   //add static methods here
+  static titleize(str) {
+    str = str.split(' ').map(word => {
+      let exceptions = ['the', 'a', 'an', 'but', 'of', 'and', 'for', 'at', 'by','from']
+      if(!exceptions.includes(word)){
+        return Formatter.capitalize(word);
+      }
+      else {
+        return word;
+      }
+    }).join(' ');
+    return Formatter.capitalize(str);
+  }
+
+  static capitalize(str) {
+    // str.split(' ').map(word => Formatter.titleize(word)).join(' ');
+    return [str.charAt(0).toUpperCase(),...str.slice(1)].join('');
+  }
+
+  static sanitize(str) {
+    return str.replace(/[^A-Za-z0-9 '-]/g,'')
+  }
+
 }
